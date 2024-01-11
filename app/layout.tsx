941654c197de3e91942d1d4f22f40ad2a14375cf
Ritvars <ritvars.biksis@gmail.com>
2024-01-11 19:51:29 +0200

    The Vercel analitycs has been added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import '@fontsource/inter/300.css'
 import '@fontsource/inter/400.css'
 import '@fontsource/inter/500.css'
+import { Analytics } from '@vercel/analytics/react'
 import { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 
@@ -18,7 +19,10 @@ const inter = Inter({ subsets: ['latin-ext'] })
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} min-w-fit`}>{children}</body>
+      <body className={`${inter.className} min-w-fit`}>
+        {children}
+        <Analytics />
+      </body>
     </html>
   )
 }
