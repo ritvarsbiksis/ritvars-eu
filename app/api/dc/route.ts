11e3413bf49c239057476dffa707518860e9f4f5
Ritvars Biksis <ritvars.biksis@tietoevry.com>
2024-05-19 22:05:57 +0300

    Data type has been updated

--- app/api/dc/route.ts
+++ app/api/dc/route.ts
@@ -1,11 +1,16 @@
 import { NextRequest, NextResponse } from 'next/server'
 
+interface ReqData {
+  event: 'decoder_output'
+  data: Record<string, string | object>
+}
+
 export async function POST(request: NextRequest) {
-  const data = (await request.json()) as { result: Record<string, string> }
+  const data = (await request.json()) as ReqData
 
   // TODO: remove logs
   console.log('data :: ', data)
-  console.log('data.result :: ', data.result)
+  console.log('data.data.result :: ', data.data.result)
 
   const msg = 'Data receive!'
   const json = {
